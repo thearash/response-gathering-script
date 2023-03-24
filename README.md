e2fb16ea74caef9c8af8bf72af77cb302ccc49a7
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-03-24 16:09:29 +0900

    init: initialize project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Reponse Gathering Script
+
+This repository contains a script for gathering responses from ChatGPT and produce a file with the responses.
+
+## Installation
+
+To use this script, you must have <a href="https://nodejs.org/en/" target="_new">Node.js</a> and <a href="https://www.npmjs.com/" target="_new">npm</a> installed on your system.
+
+1. Clone this repository to your local machine.
+2. Navigate to the repository directory in your terminal.
+3. Run `npm install` to install the necessary dependencies.
+4. Copy `.env.example` to `.env` and fill in the `OPENAI_API_KEY` which can be obtained by following [this page](https://platform.openai.com/docs/api-reference/making-requests).
+
+## Usage
+
+1. Run the script using the command `npm start -s="<SOURCE_FOLDER>"`. For example, `npm start -s="./competition"`.
+2. The script will output response files in `../<SOURCE_FOLDER>/competition/<TEAM_NAME>/<CHARACTER>` folder. The file `response_log_<DATE_TIME>.txt` will be created in the `../<SOURCE_FOLDER>/competition/logs` folder.
+
+Please ensure that the source folder contains only text files.
+
+Please note that interacting with ChatGPT via API costs money. Please refer to [this page](https://openai.com/pricing) for more information.
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import { Configuration, OpenAIApi } from 'openai'
+import { appendLog, createLogFolder, listAllFiles } from './file-utils';
+
+import dotenv from 'dotenv'
+import fs from 'fs'
+import parseArgs from 'minimist'
+import path from 'path'
+
+dotenv.config()
+
+const CHARACTER_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+  'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const NUM_TRIALS = 10
+const OBJECT_TOKEN = '<OBJECT>'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+async function main() {
+  const args = parseArgs(process.argv.slice(2))
+  const argv = process.platform === 'win32' ? args['_'] : args['s']
+  if (argv === undefined) {
+    throw Error('Insufficient parameters to work with.')
+  }
+
+  const sourceFolder = argv + '/'
+  const sFolder = path.posix.resolve(sourceFolder)
+
+  const outputDir = path.posix.join(sFolder, '..', 'competition')
+  if (!fs.existsSync(outputDir)) {
+    await fs.promises.mkdir(outputDir)
+  }
+
+  const logFolderPath = await createLogFolder(outputDir)
+
+  const promptsFiles = await listAllFiles(sFolder)
+  for (const promptFile of promptsFiles) {
+    const promptFilePath = path.posix.join(sFolder, promptFile)
+    const fileContent = await fs.promises.readFile(promptFilePath, 'utf-8')
+
+    const teamName = promptFile.split('.').slice(0, -1).join('.')
+    const teamFolderPath = path.posix.join(outputDir, teamName)
+    if (!fs.existsSync(teamFolderPath)) {
+      await fs.promises.mkdir(teamFolderPath)
+    }
+    const teamLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName}`
+    await appendLog(logFolderPath, teamLog)
+
+    for (const character of CHARACTER_LIST) {
+      const characterFolderPath = path.posix.join(teamFolderPath, character)
+      if (!fs.existsSync(characterFolderPath)) {
+        await fs.promises.mkdir(characterFolderPath)
+      }
+
+      const characterLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character}`
+      await appendLog(logFolderPath, characterLog)
+
+      const prompt = fileContent.replaceAll(OBJECT_TOKEN, `"${character}"`)
+
+      for (let i = 0; i < NUM_TRIALS; i++) {
+        let response = ''
+        try {
+          const completion = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+              "role": "user", "content": prompt,
+            }]
+          })
+          console.log(completion.data.choices)
+          response = completion.data.choices[0].message?.content.toString() || ''
+        } catch (e) {
+          const errorLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - error: ${e}`
+          await appendLog(logFolderPath, errorLog)
+          continue
+        }
+
+        if (response.length === 0) {
+          const errorLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - error: empty response`
+          await appendLog(logFolderPath, errorLog)
+          continue
+        }
+
+        const trialLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1}`
+        await appendLog(logFolderPath, trialLog)
+
+        const filePath = path.posix.join(characterFolderPath, `${teamName}_${character}_${i + 1}.txt`)
+        await fs.promises.writeFile(filePath, response)
+      }
+    }
+  }
+}
+main()
