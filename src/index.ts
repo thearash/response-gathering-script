734638b605d7027f444520ed2c5880415d45c8f3
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-03-27 13:32:27 +0900

    refactor: use package

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,61 @@
 import { Configuration, OpenAIApi } from 'openai'
-import { appendLog, createLogFolder, listAllFiles } from './file-utils';
+import { appendLog, createLogFolder, listAllFiles, parseSourceFolderArgument } from 'chatgpt4pcg-node'
 
 import dotenv from 'dotenv'
 import fs from 'fs'
-import parseArgs from 'minimist'
 import path from 'path'
 
-dotenv.config()
-
-const STAGE = 'raw'
+const CURRENT_STAGE = 'raw'
 const CHARACTER_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
   'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 const NUM_TRIALS = 10
 const OBJECT_TOKEN = '<OBJECT>'
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration)
-
 async function main() {
-  const args = parseArgs(process.argv.slice(2))
-  const argv = process.platform === 'win32' ? args['_'] : args['s']
-  if (argv === undefined) {
-    throw Error('Insufficient parameters to work with.')
+  dotenv.config()
+  
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration)
+
+  let sourceFolder = ''
+
+  try {
+    sourceFolder = parseSourceFolderArgument()
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message)
+    }
+    return
   }
 
-  const sourceFolder = argv + '/'
-  const sFolder = path.posix.resolve(sourceFolder)
-
-  const outputDir = path.posix.join(sFolder, '..', 'competition')
+  const outputDir = path.posix.join(sourceFolder, '..', 'competition')
   if (!fs.existsSync(outputDir)) {
     await fs.promises.mkdir(outputDir)
   }
 
   const logFolderPath = await createLogFolder(outputDir)
 
-  const promptsFiles = await listAllFiles(sFolder)
-  for (const promptFile of promptsFiles) {
-    const promptFilePath = path.posix.join(sFolder, promptFile)
+  const promptsFiles = await listAllFiles(sourceFolder)
+
+  if (promptsFiles.length === 0) {
+    const log = `[${new Date().toISOString()}] Processing - No prompt files found.`
+    await appendLog(logFolderPath, CURRENT_STAGE, log)
+    return
+  }
+
+  promptsFiles.forEach(async (promptFile) => {
+    const promptFilePath = path.posix.join(sourceFolder, promptFile)
     const fileContent = await fs.promises.readFile(promptFilePath, 'utf-8')
+
+    if (fileContent.length === 0) {
+      const log = `[${new Date().toISOString()}] Processing - team: ${promptFile} - error: empty file`
+      await appendLog(logFolderPath, CURRENT_STAGE, log)
+      return
+    }
 
     const teamName = promptFile.split('.').slice(0, -1).join('.')
     const teamFolderPath = path.posix.join(outputDir, teamName)
@@ -49,54 +63,59 @@ async function main() {
       await fs.promises.mkdir(teamFolderPath)
     }
 
-    const stageFolderPath = path.posix.join(teamFolderPath, STAGE)
+    const stageFolderPath = path.posix.join(teamFolderPath, CURRENT_STAGE)
     if (!fs.existsSync(stageFolderPath)) {
       await fs.promises.mkdir(stageFolderPath)
     }
 
-    const teamLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName}`
-    await appendLog(logFolderPath, teamLog)
+    const teamLog = `[${new Date().toISOString()}] Processing - team: ${teamName}`
+    await appendLog(logFolderPath, CURRENT_STAGE, teamLog)
 
-    for (const character of CHARACTER_LIST) {
+    CHARACTER_LIST.forEach(async (character) => {
       const characterFolderPath = path.posix.join(stageFolderPath, character)
       if (!fs.existsSync(characterFolderPath)) {
         await fs.promises.mkdir(characterFolderPath)
       }
 
-      const characterLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character}`
-      await appendLog(logFolderPath, characterLog)
+      const characterLog = `[${new Date().toISOString()}] Processing - team: ${teamName} - character: ${character}`
+      await appendLog(logFolderPath, CURRENT_STAGE, characterLog)
 
       const prompt = fileContent.replaceAll(OBJECT_TOKEN, `"${character}"`)
 
-      for (let i = 0; i < NUM_TRIALS; i++) {
-        let response = ''
-        try {
-          const completion = await openai.createChatCompletion({
-            model: 'gpt-3.5-turbo',
-            messages: [{
-              "role": "user", "content": prompt,
-            }]
-          })
-          response = completion.data.choices[0].message?.content.toString() || ''
-        } catch (e) {
-          const errorLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - error: ${e}`
-          await appendLog(logFolderPath, errorLog)
-          continue
-        }
+      await processTrials(openai, prompt, characterFolderPath, logFolderPath, teamName, character)
+    })
+  })
+}
 
-        if (response.length === 0) {
-          const errorLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - error: empty response`
-          await appendLog(logFolderPath, errorLog)
-          continue
-        }
-
-        const trialLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - Success`
-        await appendLog(logFolderPath, trialLog)
-
-        const filePath = path.posix.join(characterFolderPath, `${teamName}_${character}_${i + 1}.txt`)
-        await fs.promises.writeFile(filePath, response)
-      }
+async function processTrials(openai: OpenAIApi, prompt: string, characterFolderPath: string, logFolderPath: string, teamName: string, character: string) {
+  for (let i = 0; i < NUM_TRIALS; i++) {
+    let response = ''
+    try {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{
+          "role": "user", "content": prompt,
+        }]
+      })
+      response = completion.data.choices[0].message?.content.toString() || ''
+    } catch (e) {
+      const errorLog = `[${new Date().toISOString()}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - error: ${e}`
+      await appendLog(logFolderPath, CURRENT_STAGE, errorLog)
+      continue
     }
+
+    if (response.length === 0) {
+      const errorLog = `[${new Date().toISOString()}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - error: empty response`
+      await appendLog(logFolderPath, CURRENT_STAGE, errorLog)
+      continue
+    }
+
+    const trialLog = `[${new Date().toISOString()}] Processing - team: ${teamName} - character: ${character} - trial: ${i + 1} - Success`
+    await appendLog(logFolderPath, CURRENT_STAGE, trialLog)
+
+    const filePath = path.posix.join(characterFolderPath, `${teamName}_${character}_${i + 1}.txt`)
+    await fs.promises.writeFile(filePath, response)
   }
 }
+
 main()
